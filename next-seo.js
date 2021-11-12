bd554f11279bf3e8a74b836ebba280d419d269c2
Mike López <miguelernesto_23@hotmail.com>
2021-11-12 11:12:22 -0600

    [US-23] Code Styling (#3)
    * Typescript
    
    * Eslint
    
    * Prettier
    
    * Sitemaps + SEO
    
    * Json-server for mocking endpoints
    
    * README updated

---
+++ next-seo.js
@@ -0,0 +1,9 @@
+const NextSeo = {
+    openGraph: {
+        type: "website",
+        url: "https://jork-front.vercel.app/",
+        site_name: "jork-front",
+    },
+};
+
+export default NextSeo;
