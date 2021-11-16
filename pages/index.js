38841260442a50ef03fee81747e188f457d60449
Antonio Rodriguez Barrientos <44013928+antonio1998abril@users.noreply.github.com>
2021-11-16 12:57:48 -0600

    Added proxy to server (#4)

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,46 @@
+import axios from 'axios'
 import Head from 'next/head'
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({info}) {
+  const initialState = {
+    name:'',
+    email:''
+  }
+  const [formulario,setFormulario] = useState(initialState)
+  const [datos,setDatos] = useState(info)
+ 
+
+  const setdata=e=> {
+    const {name,value}= e.target
+    setFormulario({...formulario,[name]:value})
+  } 
+
+  const SubmitExample = async(e) => {
+    e.preventDefault()
+    console.log(formulario.name)
+    try{
+     /*  fetch('/demo/add', {
+        method: 'POST',
+        body: formulario,
+        headers: {'Content-type': 'application/json; charset=UTF-8'}
+    }).then(response => response.json()) */
+    const foo = await axios.post('/demo/add',{...formulario})
+
+    }catch(err){
+      alert(err)
+    }
+  }
+
+  useEffect(async() => {
+    const res = await fetch('/demo/all')
+    const info = await res.json()
+
+    setDatos(info)
+  
+}, [datos]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,39 +52,26 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js</a> on Docker!
         </h1>
+        AQUI EMPIEZA LA LISTA DE USUARIOS: 
+        {
+          datos.map(data => (
+          <div key={data.id}>
+          <h1 >{data.name}, {data.email}</h1>
+          </div>
+          ))
+        }
+      <form onSubmit={SubmitExample}>
+        <label>Ingresa name: </label>
+        <input placeholder="Ingresa nombre" name="name" onChange={setdata}></input>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
+        <label>Ingresa email: </label>
+        <input placeholder="Ingresa el email" name="email" onChange={setdata}></input>
+        <button type="submit" >ENVIAR</button>
+      </form>
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
           </a>
         </div>
       </main>
@@ -62,4 +88,13 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export const getServerSideProps = async() => {
+  const res = await fetch('http://localhost:8080/demo/all')
+  const info = await res.json()
+
+  return {
+    props:{info}
+  }
 }
