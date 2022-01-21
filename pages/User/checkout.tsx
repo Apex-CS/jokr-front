c952a59f8c6ea38a39e2b70f9acb082d1be40078
Roberto Lazo <roberto.lazo@alumnos.udg.mx>
2022-01-21 01:27:19 -0600

    Add fild of addresses in the page checkout

--- pages/User/checkout.tsx
+++ pages/User/checkout.tsx
@@ -1,17 +1,109 @@
 import { TodosContext } from '@/components/contexts';
 import { loadStripe } from '@stripe/stripe-js';
-import React, { useContext } from 'react';
-import { Card, Typography, CardMedia, CardContent, CardActionArea,Container, Button, TextField} from '@mui/material';
+import React, { useContext, useEffect, useState } from 'react';
+import {
+  Card,
+  Typography,
+  CardMedia,
+  CardContent,
+  CardActionArea,
+  Container,
+  Button,
+  TextField,
+  Grid,
+} from '@mui/material';
 import { CartItemType } from '@/pages/index';
+import data from './address.json';
+import axios from 'axios';
+import swal from 'sweetalert';
+import Router from 'next/router';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
+function refreshPage() {
+  window.location.reload();
+}
 
+interface ShippingData {
+  id: string;
+  recipient_name: string;
+  colonia: string;
+  country: string;
+  municipio: string;
+  phone: string;
+  postal_code: string;
+  state: string;
+  street1: string;
+  street2: string;
+}
 
-import data from './address.json'
+export type FieldCategory = {
+  id: number;
+  recipient_name: string;
+  colonia: string;
+  country: string;
+  municipio: string;
+  phone: string;
+  postal_code: string;
+  state: string;
+  street1: string;
+  street2: string;
+  categories: {
+    id: number;
+    name: string;
+  };
+};
+
 const stripePromise = loadStripe(
   'pk_test_51KCANHGz1RvjBCqBCgQwXY9h7TcmatIeXDbz67BmITIxQElhfoHHl1avDUGAgpGiRp7SPrwsap73ETmnx8gS3OGD00Xwbg2kjb'
 );
 
+const fetcher = (url: string) => axios.get(url).then((res) => res.data);
+
 function Checkout() {
+  const { callback, isCallback } = useContext(TodosContext);
+  const [data, setData] = useState([]);
+  const { IdUser } = useContext(TodosContext);
+  const [adress, setAddress] = React.useState('');
+  const [adressName, setAddressName] = React.useState('Recipient Name');
+  const [adressPhone, setAddressPrhone] = React.useState('Phone Number');
+  const [adressStreetA, setAddressStreetA] = React.useState('Street name 1');
+  const [adressStreetB, setAddressStreetB] = React.useState('Street name 2');
+  const [adressPostal, setAddressPostal] = React.useState('Postal/Zip Code');
+  const [adressCity, setAddressCity] = React.useState('City');
+  const [adressState, setAddressState] = React.useState('Province/State');
+  const [adressCountry, setAddressCountry] = React.useState('Country');
+  const handleChange = (event: SelectChangeEvent) => {
+    setAddress(event.target.value as string);
+
+
+    setAddressName(event.target.value as string);
+  };
+
+  useEffect(() => {
+    isCallback(!callback);
+    const AllAddressesFunction = async () => {
+      try {
+        const res = await axios.get(`/api/v1/addresses/${IdUser}`, {
+          headers: { Authorization: 'Bearer ' + localStorage.getItem('token') },
+        });
+        if (res) {
+          setData(res.data);
+        }
+        console.log(res.data);
+      } catch (err) {
+        swal({ icon: 'error', text: 'Session Expired', timer: 20000 }).then(function () {
+          localStorage.removeItem('token');
+          Router.push('/login');
+        });
+      }
+    };
+    AllAddressesFunction();
+  }, []);
+
   const { cartItems } = useContext(TodosContext);
   const handleClick = async (event: any) => {
     const { sessionId } = await fetch('/api/checkout/session', {
@@ -31,120 +123,107 @@ function Checkout() {
   const calculateTotal = (items: CartItemType[]) =>
     items.reduce((ack: number, item) => ack + item.amount * item.price, 0);
 
-
   return (
-    <div >
+    <div>
       <Container>
+        <Typography variant="h3" gutterBottom align="center">
+          Checkout Page
+        </Typography>
+        <div className="grid1">
+          <Typography variant="h5" gutterBottom>
+            Products
+          </Typography>
+          {cartItems?.map((item: CartItemType) => {
+            return (
+              <Card sx={{ maxWidth: 345, marginBottom: '1rem' }} key={item.id}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="140"
+                    image={item.photoUrl}
+                    alt="green iguana"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Title: {item.name}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Description: {item.description} <br />
+                      Amount: {item.amount} <br />
+                      Price: {item.price} <br />
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            );
+          })}
+          <Typography variant="h6" gutterBottom>
+            Total: ${calculateTotal(cartItems).toFixed(2)}
+          </Typography>
 
-        <div className='grid1'>
-      {cartItems?.map((item: CartItemType) => {
-        return (
-          <Card sx={{  maxWidth: 345, marginBottom: '1rem' }} key={item.id}>
-            <CardActionArea>
-              <CardMedia component="img" height="140" image={item.photoUrl} alt="green iguana" />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Title: {item.name}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Description: {item.description} <br />
-                  Amount: {item.amount} <br />
-                  Price: {item.price} <br />
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        );
-      })}
-      Total: ${calculateTotal(cartItems).toFixed(2)}
-      <h1> Checkout</h1>
-      {cartItems.length == 0 ? null : (
-        <Button className='Checkout' role="link" onClick={handleClick}>
-          Checkout
-        </Button>
-      )}
-
-
-
-
-      </div>
-      
-      <div className="grid2">
-
-
-AUI VA EL FORMULARIO solo dale una buena vista
-
-
-
-     
-        
-          <form className="login100-form " /* onSubmit={handleSubmit} */>
-
-
-            <div className="container">
-              <div className="top-header">
-                <h3>Welcome back</h3>
-                <p>Enter your credentials to access your account</p>
-              </div>
-
-        
-              <TextField
-               /*  error={state.isError} */
-                fullWidth
-                id="username"
-                type="email"
-                label="Username"
-                placeholder="Username"
-                margin="normal"
-              /*   onChange={handleUsernameChange} */
-                size="small"
-                autoComplete="on"
-              />
-
-              <TextField
-               /*  error={state.isError} */
-                fullWidth
-                id="password"
-                type="EXAMPLE"
-                label="EXAMPLE"
-                placeholder="Ubicaicon"
-                margin="normal"
-/*                 helperText={state.helperText}
-                onChange={handlePasswordChange} */
-                size="small"
-                autoComplete="on"
-              />
-
-     
-
-              <div className="btn">
-                {/* <button  >Sign in</button> */}
-                <Button
-                  variant="contained"
-                  size="large"
-                  color="secondary"
-    /*               onClick={handleLogin}
-                  disabled={state.isButtonDisabled} */
-                >
-                 Sign in
-                </Button>
-              </div>
-            </div>
-            <p className="last">
-            EXA,MPLE
-            </p>
-          </form>
-
-          <div className="login100-more" />
+          {cartItems.length == 0 ? null : (
+            <Button className="Checkout" role="link" variant="contained" onClick={handleClick}>
+              Checkout
+            </Button>
+          )}
         </div>
-    
- 
 
-  
+        <div className="grid2">
+          <Typography variant="h5" gutterBottom>
+            Shipping Address
+          </Typography>
 
-</Container>
+          <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Address</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={adress}
+                label="Address"
+                onChange={handleChange}
+              >
+                {data?.map((field: FieldCategory) => {
+                  return (
+                    <MenuItem key={field.id} value={field.id} >
+                      {field.street1 + ' Col. ' + field.colonia}{' '}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            </FormControl>
+          </Box>
 
-
+          <form noValidate autoComplete="off">
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField label= {adressName} variant="outlined" disabled={true} fullWidth  />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label={adressPhone} variant="outlined" disabled={true} fullWidth />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField label={adressStreetA} variant="outlined" disabled={true} fullWidth />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField label={adressStreetB} variant="outlined" disabled={true} fullWidth />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label={adressPostal} variant="outlined" disabled={true} fullWidth />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label={adressCity} variant="outlined" disabled={true} fullWidth />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label={adressState} variant="outlined" disabled={true}fullWidth />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField label={adressCountry} variant="outlined" disabled={true} fullWidth />
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Container>
     </div>
   );
 }
